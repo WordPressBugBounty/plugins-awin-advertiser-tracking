44f18b13758683e609a826565cabb923127bde64
Sean Murphy <sean@iamseanmurphy.com>
2025-01-27 18:18:45 +0000

    1.3.1

--- awin-advertiser-tracking/awin-advertiser-tracking.php
+++ awin-advertiser-tracking/awin-advertiser-tracking.php
@@ -4,7 +4,7 @@
  * Plugin Name: Awin - Advertiser Tracking
  * Plugin URI: https://wordpress.org/plugins/awin-advertiser-tracking
  * Description: The Awin Advertiser Tracking plugin allows for seamless integration of our core Advertiser Tracking Suite within WooCommerce.
- * Version: 1.2.1
+ * Version: 1.3.1
  * Author: awinglobal
  * Author URI: https://profiles.wordpress.org/awinglobal/
  * Text Domain:  awin-advertiser-tracking
@@ -14,7 +14,7 @@
  * License: ModifiedBSD
  */
 
-define('AWIN_ADVERTISER_TRACKING_VERSION', '1.2.1');
+define('AWIN_ADVERTISER_TRACKING_VERSION', '1.3.1');
 define('AWIN_SLUG', 'awin_advertiser_tracking');
 define('AWIN_TEXT_DOMAIN', 'awin-advertiser-tracking');
 define('AWIN_SETTINGS_KEY', 'awin_settings');
--- awin-advertiser-tracking/readme.txt
+++ awin-advertiser-tracking/readme.txt
@@ -7,9 +7,9 @@ Tags:              awin, affiliate, zanox, affiliate window, advertiser, trackin
 Author URI:        https://profiles.wordpress.org/awinglobal/
 Author:            awinglobal
 Requires at least: 3.5
-Tested up to:      6.4
-Stable tag:        1.2.1
-Version:           1.2.1
+Tested up to:      6.7.1
+Stable tag:        1.3.1
+Version:           1.3.1
 Requires PHP:      7.3
 
 
@@ -50,7 +50,7 @@ For the purpose of attributing sales and commissions to publishers, Awin will pr
 
 == Changelog ==
 
-= 1.2.1 =
+= 1.3.1 =
 * Added cron job for generating product feed.
 * Fixed bug with Fall-back conversion pixel not firing due to lazy loading of images
 * Improved cookie handling
